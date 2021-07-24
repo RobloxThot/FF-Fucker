532c2ed3ad5d449283411c3ec031a76bc0ac0622
tr3iscool <tresiefers@gmail.com>
2021-07-24 14:50:13 -0400

    add some comments to YTTools.py

--- imports/YouTubeTools.py
+++ imports/YouTubeTools.py
@@ -1,17 +1,23 @@
 import urllib.request, json, urllib
 
 def getTitle(videoIdOrLink):
-    """Get the title of a YouTube Video"""
+    """
+    Get the title of a YouTube Video
+    """
+
+    #Check if its a ID or a link
     if "https://" in videoIdOrLink:
         videoLink = videoIdOrLink
     else:
         videoLink = "https://www.youtube.com/watch?v=%s" % videoIdOrLink
 
+    #Setup to get the title i think (This was taken from stackoverflow)
     params = {"format": "json", "url": videoLink}
     url = "https://www.youtube.com/oembed"
     query_string = urllib.parse.urlencode(params)
     url = url + "?" + query_string
 
+    #Send request to get the title
     with urllib.request.urlopen(url) as response:
         response_text = response.read()
         data = json.loads(response_text.decode())
