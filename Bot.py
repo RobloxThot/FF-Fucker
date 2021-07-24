6caf3b8e783910b75b2fa5781bbded3bc01e6e5c
tr3iscool <tresiefers@gmail.com>
2021-07-23 23:30:07 -0400

    Change a thing that did a thing

--- Bot.py
+++ Bot.py
@@ -34,7 +34,7 @@ def convert_bytes(num):
     """
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
         if num < 1024.0:
-            return "%3.1f %s" % (num, x)
+            return "%3.2f %s" % (num, x)
         num /= 1024.0
 
 def file_size(file_path):
